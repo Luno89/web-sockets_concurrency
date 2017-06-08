81d94bf3f49ae54a7ca7e395112d98127eade63a
Corigan Simpson <corigansimpson@gmail.com>
2017-06-08 00:14:24 -0500

    adding tests to implement websockets

--- producer_test/producer_test.py
+++ producer_test/producer_test.py
@@ -1,4 +1,6 @@
+import websocket
 from nose.tools import *
+from mock import MagicMock
 import producer
 
 
@@ -12,3 +14,11 @@ def teardown():
 
 def test_basic():
     assert True
+
+def makes_data():
+    web_socket = websocket.WebSocket()
+    web_socket.send = MagicMock()
+
+    # producer.produce(100)
+
+    web_socket.send.assert_called_once_with(1)
